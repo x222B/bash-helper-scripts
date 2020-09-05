62444e40f23b77a437e2726d02bfe139ebe277a0
Dino Tahto <dinotahto.dev@gmail.com>
2020-09-06 01:08:17 +0200

    Cleanup old files

--- README.md
+++
@@ -1,6 +0,0 @@
-Just a collection of some of my bash scripts for various purposes (backups,
-testing, utilities, etc.)
-
-Most of them are called by other programs, deprecated, or called manually. 
-I usually keep them in bin directory but keep them in separate repo for
-management.
--- paclist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# paclist - creates list of all installed packages
-# reinstall with pacman -S $(cat pacman)
-
-pacman -Qqet | grep -v "$(pacman -Qqg base)" | grep -v "$(pacman -Qqm)" > /$HOME/.pkglist/pacman
-
-# A list of local packages (includes AUR and locally installed)
-pacman -Qqm > /$HOME/.pkglist/aur
